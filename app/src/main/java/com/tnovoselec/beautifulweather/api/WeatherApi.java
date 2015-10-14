cf9ef41f92cb961ea31e9543b93f5878e9ced327
Tomislav Novoselec <tomislav.novoselec@fiveminutes.eu>
2015-10-14 22:17:17 +0200

    added api key

--- app/src/main/java/com/tnovoselec/beautifulweather/api/WeatherApi.java
+++ app/src/main/java/com/tnovoselec/beautifulweather/api/WeatherApi.java
@@ -8,7 +8,8 @@ import rx.Observable;
 
 public interface WeatherApi {
 
+  String API_KEY = "b8a9c2bc8b76cd26c47294f2169c26d0";
 
-  @GET("/data/2.5/forecast/?units=metric")
+  @GET("/data/2.5/forecast/?units=metric&APPID=" + API_KEY)
   Observable<HourlyForecast> getForecast(@Query("lat") double latitude, @Query("lon") double longitude);
 }
